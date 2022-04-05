ebefc19ce211ca305e642700210108e60186e0f7
BrianCaetano <brian-caetano@hotmail.com>
2022-04-05 18:24:56 -0300

    refatora desafio 9

--- challenges/desafio9.js
+++ challenges/desafio9.js
@@ -1,3 +1,3 @@
 db.produtos.find({
-  "valoresNutricionais.0.quantidade": { $lt: 500 },
+  valoresNutricionais: { $elemMatch: { tipo: "calorias", quantidade: { $lt: 500 } } },
 }, { _id: false, nome: true });
